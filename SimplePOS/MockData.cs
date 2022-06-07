fef1d99cbd3ddcbf0c262804aa4c828c45bc6303
Grace <grace@datameshgroup.com>
2022-06-07 16:43:44 +1000

    Added handling of mock data.
    Added handling of mock data.  This is currently used to load SaleItems from mockdata.json file, which will be useful for Mock Host Integration testing. Also removed unwanted blank lines.

---
+++ SimplePOS/MockData.cs
@@ -0,0 +1,40 @@
+﻿using DataMeshGroup.Fusion.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePOS
+{
+    public class MockData
+    {
+        [JsonIgnore]
+        public List<SaleItem> SaleItems { get; set; }
+
+        public List<PaymentSaleItem> PaymentSaleItems {
+            set 
+            {
+                SaleItems = new List<SaleItem>();
+                if (value != null)
+                {
+                    foreach (PaymentSaleItem item in value)
+                    {
+                        SaleItems.Add(new SaleItem() { ProductCode = item.ProductCode, ProductLabel = item.ProductLabel, ItemAmount = item.ItemAmount });
+                    }
+                }
+            }  
+        }
+
+        public class PaymentSaleItem
+        {
+            public string ProductCode;
+            public string ProductLabel;
+            public decimal ItemAmount;
+        }
+    }
+
+
+}
