178cd2c18fa80458841bd77ca07208135583a5bc
Grace <95324995+i-grace@users.noreply.github.com>
2023-08-18 11:54:42 +1000

    Updated UI display for multiple terminal support.  Updated Sale Item mockdata handling.  Added sample mockdata.json file - should be place in the same directory as the compiled exe, when used.

--- SimplePOS/MockData.cs
+++ SimplePOS/MockData.cs
@@ -14,34 +14,18 @@ namespace SimplePOS
         [JsonIgnore]
         public List<SaleItem> SaleItems { get; set; }
 
-        public List<PaymentSaleItem> PaymentSaleItems {
+        public List<SaleItem> PaymentSaleItems {
             set 
             {
                 SaleItems = new List<SaleItem>();
                 if (value != null)
                 {
-                    foreach (PaymentSaleItem item in value)
+                    foreach (SaleItem item in value)
                     {
-                        SaleItem si = new SaleItem();
-                        si = new SaleItem() { ProductCode = item.ProductCode, ProductLabel = item.ProductLabel, ItemAmount = item.ItemAmount };
-                        si.Categories = new List<string>();
-                        si.Category = item.Category;
-                        si.SubCategory = item.SubCategory;
-                        SaleItems.Add(si);
+                        SaleItems.Add(item);
                     }
                 }
             }  
         }
-
-        public class PaymentSaleItem
-        {
-            public string ProductCode;
-            public string ProductLabel;
-            public decimal ItemAmount;
-            public string Category;
-            public string SubCategory;
-        }
     }
-
-
 }
